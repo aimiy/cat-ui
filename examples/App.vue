63ebad4e3911b2afb0b4533fbb13ae748d2a2e6d
aimiy <mioly@foxmail.com>
2019-11-09 14:43:06 +0800

    catui

---
+++ examples/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <!-- <router-link to="/Button">Button</router-link> -->
+    <!-- <router-link ></router-link> -->
+    <!-- <Button/> -->
+    <!-- <Toast/> -->
+    <!-- <FoldList/> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+  },
+  created(){
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
